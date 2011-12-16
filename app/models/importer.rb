859cf225f2913ac77142736c9a9d8f7b05d5fd59
Jason <jasonmichaelroth@gmail.com>
2011-12-16 09:00:40 -0700

    added the Importer model

---
+++ app/models/importer.rb
@@ -0,0 +1,71 @@
+require 'csv'
+
+class Importer
+  include ActiveModel::Validations
+
+  attr_accessor :tab_file
+  validates :tab_file, presence: true
+  validate :verify_tab_file, unless: 'tab_file.blank?'
+
+  def import(file)
+    @tab_file = file
+    return false unless valid?
+
+    Rails.logger.info "Importing #{@tab_file}"
+
+    file = File.read(@tab_file)
+    csv = CSV.parse(file, {col_sep: "\t", headers: true})
+
+    ActiveRecord::Base.transaction do
+      csv.each_with_index do |row,index|
+        # account for the header, and the zero index
+        @csv_line = index+2
+
+        purchaser = Purchaser.find_or_create_by_name(name: row[0])
+
+        merchant = Merchant.find_or_create_by_name_and_address(
+          address: row[4],
+          name: row[5]
+        )
+
+        item = merchant.items.find_or_create_by_description_and_price(
+          description: row[1],
+          price: row[2]
+        )
+
+        purchase = Purchase.create(
+          num_items: row[3],
+          purchaser: purchaser,
+          item: item
+        )
+
+        # Trigger a rollback if any of the models are invalid.
+        # This invalidates the entire imported file, which is intended. This can
+        # be refactored to serve as a filter for valid rows, but then it should
+        # be abstracted to it's own model for better control and reporting.
+        purchaser.save! and merchant.save! and item.save! and purchase.save!
+
+      end
+    end
+
+    true
+
+  rescue ActiveRecord::RecordInvalid
+    errors.add(:tab_file, "has an invalid row at line #{@csv_line}")
+    false
+
+  rescue ArgumentError, CSV::MalformedCSVError, Errno::ENOENT
+    errors.add(:tab_file, 'is not a tab deliminated file')
+    false
+  end
+
+
+  private
+
+  def verify_tab_file
+    unless File.exists?(tab_file)
+      errors.add(:tab_file, 'does not exist')
+    end
+  end
+
+end
---
+++ spec/models/importer_spec.rb
@@ -0,0 +1,93 @@
+require 'spec_helper'
+
+describe Importer do
+  before do
+    @it = Importer.new
+  end
+
+  it "reads and writes the tab file" do
+    @it.tab_file = "/path/to/something"
+    @it.tab_file.should eq "/path/to/something"
+  end
+
+  it "requires a tab file" do
+    @it.tab_file = nil
+    @it.valid?.should be_false
+    @it.errors[:tab_file].should_not be_empty
+  end
+
+  describe '#import' do
+
+    context "with no file" do
+      before do
+        @file = "/path/to/nowhere.png"
+      end
+
+      it "returns false and sets an error" do
+        @it.import(@file).should be_false
+        @it.errors[:tab_file].should_not be_empty
+      end
+    end
+
+    context "with an invalid file (e.g. an image)" do
+      before do
+        @file = File.join(Rails.root, 'spec/fixtures/rails.png')
+      end
+
+      it "returns false and sets an error" do
+        @it.import(@file).should be_false
+        @it.errors[:tab_file].should_not be_empty
+      end
+
+      it "saves nothing" do
+        @it.import(@file).should be_false
+        Purchaser.count.should be_zero
+        Merchant.count.should be_zero
+        Item.count.should be_zero
+        Purchase.count.should be_zero
+      end
+    end
+
+    context "with a valid file with bad data" do
+      before do
+        @file = File.join(Rails.root, 'spec/fixtures/bad_data.tab')
+      end
+
+      it "returns false and set an error" do
+        @it.import(@file).should be_false
+        @it.errors[:tab_file].should_not be_empty
+
+        # Check that the error message notifies us of the correct error line
+        @it.errors[:tab_file].to_s.should match /at line 2/
+      end
+
+      it "saves nothing" do
+        @it.import(@file).should be_false
+        Purchaser.count.should be_zero
+        Merchant.count.should be_zero
+        Item.count.should be_zero
+        Purchase.count.should be_zero
+      end
+    end
+
+    context "with a valid file with good data" do
+      before do
+        @file = File.join(Rails.root, 'spec/fixtures/good_data.tab')
+      end
+
+      it "returns true" do
+        @it.import(@file).should be_true
+      end
+
+      it "saves some records" do
+        @it.import(@file).should be_true
+        Purchaser.count.should_not be_zero
+        Merchant.count.should_not be_zero
+        Item.count.should_not be_zero
+        Purchase.count.should_not be_zero
+      end
+    end
+
+  end
+
+end
